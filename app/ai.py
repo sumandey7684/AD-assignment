6f6620af3d5cbc70b2974ba213f5b5f260c5b0d4
Suman Dey <sumandey7684@gmail.com>
2025-05-03 16:09:00 +0530

    Add files via upload

---
+++ app/ai.py
@@ -0,0 +1,12 @@
+
+from textblob import TextBlob
+
+def analyze_sentiment(text):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    if polarity > 0:
+        return 'POSITIVE'
+    elif polarity < 0:
+        return 'NEGATIVE'
+    else:
+        return 'NEUTRAL'
